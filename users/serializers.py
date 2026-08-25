fc598160f11b9a2d821058df03c7a3c6d4187121
tray <at-box@mail.ru>
2026-08-25 21:21:20 +0500

    dev: PaymentSerializer added

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from users.models import User
+from users.models import User, Payment
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,3 +11,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("email", "username", "first_name", "last_name", "phone", "city", "avatar")
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Payment model
+    """
+
+    class Meta:
+        model = Payment
+        fields = "__all__"
