eb190390ef410d51ccf79dc91f4d4326c7256a5b
tray <at-box@mail.ru>
2026-08-25 21:21:35 +0500

    dev: PaymentListAPIView added

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
-from rest_framework.generics import UpdateAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from rest_framework.generics import ListAPIView, UpdateAPIView
 from rest_framework.parsers import FormParser, MultiPartParser
 
-from users.models import User
-from users.serializers import UserSerializer
+from users.models import User, Payment
+from users.serializers import UserSerializer, PaymentSerializer
 
 
 # Create your views here.
@@ -10,3 +12,10 @@ class UserUpdateAPIView(UpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     parser_classes = [MultiPartParser, FormParser]
+
+class PaymentListAPIView(ListAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = (DjangoFilterBackend, OrderingFilter,)
+    filterset_fields = ("course", "lesson", "course__title", "lesson__title", "method")
+    ordering_fields = ("date",)
