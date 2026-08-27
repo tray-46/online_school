3762d1d79485f294e8852f0b5753a23e417910d5
tray <at-box@mail.ru>
2026-08-27 20:30:14 +0500

    dev: Courses and Lessons permissions for moderators implemented

--- lms/views.py
+++ lms/views.py
@@ -1,9 +1,12 @@
+from rest_framework.decorators import permission_classes
 from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView
 from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from lms.models import Course, Lesson
 from lms.serializers import CourseSerializer, LessonSerializer
+from users.permissions import IsModerator
 
 
 # Create your views here.
@@ -12,9 +15,18 @@ class CourseViewSet(ModelViewSet):
     serializer_class = CourseSerializer
     parser_classes = [MultiPartParser, FormParser]
 
+    def get_permissions(self):
+        if self.action in ["create", "destroy"]:
+            self.permission_classes = [IsAuthenticated, ~IsModerator]
+        else:
+            self.permission_classes = [IsAuthenticated]
+
+        return super().get_permissions()
+
 
 class LessonCreateAPIView(CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, ~IsModerator]
     parser_classes = [MultiPartParser, FormParser]
 
 
@@ -36,3 +48,4 @@ class LessonUpdateAPIView(UpdateAPIView):
 
 class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated, ~IsModerator]
