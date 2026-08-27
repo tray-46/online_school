0d3fc5a98805a8a142f47bcd0130024cb26837a5
tray <at-box@mail.ru>
2026-08-27 22:02:47 +0500

    dev: authors permissions implemented in views

--- lms/views.py
+++ lms/views.py
@@ -6,22 +6,33 @@ from rest_framework.viewsets import ModelViewSet
 
 from lms.models import Course, Lesson
 from lms.serializers import CourseSerializer, LessonSerializer
-from users.permissions import IsModerator
+from users.permissions import IsModerator, IsAuthor
 
 
 # Create your views here.
 class CourseViewSet(ModelViewSet):
-    queryset = Course.objects.all()
+    # queryset = Course.objects.all()
     serializer_class = CourseSerializer
     parser_classes = [MultiPartParser, FormParser]
 
     def get_permissions(self):
-        if self.action in ["create", "destroy"]:
+        if self.action == "create":
             self.permission_classes = [IsAuthenticated, ~IsModerator]
+        elif self.action == "destroy":
+            self.permission_classes = [IsAuthenticated, ~IsModerator, IsAuthor]
         else:
-            self.permission_classes = [IsAuthenticated]
+            self.permission_classes = [IsAuthenticated, IsModerator | IsAuthor]
 
         return super().get_permissions()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        is_moderator = IsModerator()
+        if is_moderator.has_permission(self.request, self):
+            return Course.objects.all()
+        return Course.objects.filter(author=self.request.user)
 
 
 class LessonCreateAPIView(CreateAPIView):
@@ -29,23 +40,35 @@ class LessonCreateAPIView(CreateAPIView):
     permission_classes = [IsAuthenticated, ~IsModerator]
     parser_classes = [MultiPartParser, FormParser]
 
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
 
 class LessonListAPIView(ListAPIView):
-    queryset = Lesson.objects.all()
+    # queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsModerator | IsAuthor]
+
+    def get_queryset(self):
+        is_moderator = IsModerator()
+        if is_moderator.has_permission(self.request, self):
+            return Lesson.objects.all()
+        return Lesson.objects.filter(author=self.request.user)
 
 
 class LessonRetrieveAPIView(RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsModerator | IsAuthor]
 
 
 class LessonUpdateAPIView(UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsModerator | IsAuthor]
     parser_classes = [MultiPartParser, FormParser]
 
 
 class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated, ~IsModerator]
+    permission_classes = [IsAuthenticated, ~IsModerator, IsAuthor]
