4931cf784dccbf8b472f487f45a7585a95abb176
tray <at-box@mail.ru>
2026-08-27 22:34:05 +0500

    dev: user account permissions implemented

--- users/permissions.py
+++ users/permissions.py
@@ -1,3 +1,4 @@
+from rest_framework import permissions
 from rest_framework.permissions import BasePermission
 
 
@@ -17,3 +18,11 @@ class IsAuthor(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return bool(request.user.is_authenticated and obj.author == request.user)
+
+
+class IsAccountOwner(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj == request.user
--- users/views.py
+++ users/views.py
@@ -1,15 +1,13 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.parsers import FormParser, MultiPartParser
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView as TOPView, TokenRefreshView as TRView
 
-from lms.models import Payment
-from lms.serializers import PaymentSerializer
+
 from users.models import User
+from users.permissions import IsAccountOwner
 from users.serializers import UserSerializer, UserCreateSerializer, TokenObtainPairSerializer, \
-    TokenRefreshSerializer
+    TokenRefreshSerializer, UserDetailSerializer
 
 
 # Create your views here.
@@ -30,17 +28,26 @@ class UserListAPIView(ListAPIView):
 
 class UserRetrieveAPIView(RetrieveAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    # serializer_class = UserSerializer
+
+    def get_serializer_class(self):
+        user_profile = self.get_object()
+        user = self.request.user
+        if user_profile == user:
+            return UserDetailSerializer
+        return UserSerializer
 
 
 class UserUpdateAPIView(UpdateAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = UserDetailSerializer
+    permission_classes = [IsAuthenticated, IsAccountOwner]
     parser_classes = [MultiPartParser, FormParser]
 
 
 class UserDestroyAPIView(DestroyAPIView):
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, IsAccountOwner]
 
 
 class TokenObtainPairView(TOPView):
@@ -51,21 +58,3 @@ class TokenObtainPairView(TOPView):
 class TokenRefreshView(TRView):
     serializer_class = TokenRefreshSerializer
     permission_classes = (AllowAny,)
-
-
-class PaymentListAPIView(ListAPIView):
-    queryset = Payment.objects.all()
-    serializer_class = PaymentSerializer
-    filter_backends = (
-        SearchFilter,
-        DjangoFilterBackend,
-        OrderingFilter,
-    )
-    search_fields = (
-        "course__title",
-        "lesson__title",
-        "method",
-        "date",
-    )
-    ordering_fields = ("date",)
-    filterset_fields = ("course", "lesson", "course__title", "lesson__title", "method")
