df501e53963526b3ddafc94a9decd46ed9c9f4bc
tray <at-box@mail.ru>
2026-09-09 10:37:13 +0500

    tests for stripe calls added

--- lms/tests.py
+++ lms/tests.py
@@ -5,8 +5,15 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from lms.models import Course, CourseSubscription, Lesson
+import stripe
+import unittest
+from unittest.mock import patch, MagicMock
+
+import lms.services
+from lms.models import Course, CourseSubscription, Lesson, Payment
 from users.models import User
+
+from lms.services import create_stripe_product, create_stripe_price, create_stripe_checkout_session
 
 
 # Create your tests here.
@@ -236,3 +243,131 @@ class CourseSubscriptionTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, result)
         self.assertEqual(CourseSubscription.objects.count(), 0)
+
+
+class StripeProductCreationTest(unittest.TestCase):
+
+    @patch("stripe.Product.create")
+    def test_create_stripe_product_success(self, mock_product_create):
+        mock_product_response = {
+            "id": "prod_12345",
+            "object": "product",
+            "active": True,
+            "name": "test product",
+        }
+        mock_product_create.return_value = mock_product_response
+
+        result = create_stripe_product(product_name="test product")
+
+        mock_product_create.assert_called_once_with(name="test product")
+
+        self.assertEqual(result["id"], "prod_12345")
+        self.assertEqual(result["name"], "test product")
+
+    def test_create_stripe_product_missing_name(self):
+        with self.assertRaises(ValueError):
+            create_stripe_product(product_name="")
+
+    @patch("stripe.Product.create")
+    def test_create_stripe_product_stripe_error(self, mock_product_create):
+        mock_product_create.side_effect = stripe.error.InvalidRequestError(
+            message="Invalid parameters",
+            param="name"
+        )
+
+        with self.assertRaises(stripe.error.StripeError):
+            create_stripe_product(product_name="Bad name")
+
+
+class StripePriceCreationTest(unittest.TestCase):
+
+    @patch("stripe.Price.create")
+    def test_create_price_success(self, mock_price_create):
+        mock_product = MagicMock()
+        mock_product.id = "prod_12345"
+
+        mock_price_response = {
+            "id": "price_12345",
+            "object": "price",
+            "product": "prod_12345",
+            "active": True,
+            "unit_amount": 1000,
+            "currency": "rub",
+        }
+        mock_price_create.return_value = mock_price_response
+
+        result = create_stripe_price(
+            product=mock_product,
+            amount=10,
+            currency="rub"
+        )
+
+        mock_price_create.assert_called_once_with(product=mock_product.id, unit_amount=1000, currency="rub")
+        self.assertEqual(result["id"], "price_12345")
+        self.assertEqual(result["product"], mock_product.id)
+        self.assertEqual(result["unit_amount"], 1000)
+
+    @patch("stripe.Price.create")
+    def test_create_stripe_price_stripe_error(self, mock_price_create):
+        mock_product = MagicMock()
+        mock_product.id = "prod_12345"
+
+        mock_price_create.side_effect = stripe.error.InvalidRequestError(
+            message="Request req_12345: No such product: 'prod_12345'", param="product"
+        )
+
+        with self.assertRaises(stripe.error.StripeError):
+            create_stripe_price(product=mock_product, amount=10)
+
+
+class StripeCheckoutSessionCreateTest(unittest.TestCase):
+
+    @patch("stripe.checkout.Session.create")
+    def test_create_checkout_session_success(self, mock_session_create):
+        mock_price = MagicMock()
+        mock_price.id = "price_12345"
+        mock_price.unit_amount = 100
+
+        mock_user = MagicMock()
+        mock_user.id = 1
+
+        mock_session_response = {
+            "id": "cs_12345",
+            "object": "checkout.session",
+            "mode": "payment",
+            "amount_total": 100,
+            "status": "open",
+            "url": "https://checkout.stripe.com/c/pay/cs_12345",
+            "metadata": {
+                "user_id": "1"
+            },
+        }
+        mock_session_create.return_value = mock_session_response
+
+        result = create_stripe_checkout_session(mock_price, mock_user.id)
+
+        mock_session_create.assert_called_once_with(
+            line_items=[{"price": "price_12345", "quantity": 1, }],
+            mode="payment",
+            success_url="http://127.0.0.1:8000/payments/{CHECKOUT_SESSION_ID}/",
+            cancel_url="http://127.0.0.1:8000/",
+            metadata={"user_id": "1"}
+        )
+        self.assertEqual(result["id"], "cs_12345")
+        self.assertEqual(result["metadata"]["user_id"], str(mock_user.id))
+
+    @patch("stripe.checkout.Session.create")
+    def test_create_stripe_price_stripe_error(self, mock_session_create):
+        mock_price = MagicMock()
+        mock_price.id = "price_12345"
+        mock_price.unit_amount = 100
+
+        mock_user = MagicMock()
+        mock_user.id = 1
+
+        mock_session_create.side_effect = stripe.error.InvalidRequestError(
+            message="Request req_12345: No such price: 'price_12345'", param="price"
+        )
+
+        with self.assertRaises(stripe.error.StripeError):
+            create_stripe_checkout_session(price=mock_price, user_id=mock_user.id)
