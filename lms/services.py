1a871274fce5ee0a4636dac2e118d418d5f4dc59
tray <at-box@mail.ru>
2026-09-12 21:51:00 +0500

    send_course_update_notification added

--- lms/services.py
+++ lms/services.py
@@ -1,5 +1,15 @@
+# import os
+# import django
+#
+# # Replace 'your_project_name' with the folder name containing your settings.py
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+# django.setup()
+
 import stripe
 from django.conf import settings
+from django.core import mail
+
+from lms.models import CourseSubscription
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -51,12 +61,30 @@ def create_stripe_checkout_session(price: stripe.Price, user_id: int) -> stripe.
         raise e
 
 
-def get_stripe_checkout_session_info(session_id: str) -> stripe.checkout.Session:
+def get_stripe_checkout_session_info(session_id: int) -> stripe.checkout.Session:
     try:
         checkout_session = stripe.checkout.Session.retrieve(session_id)
         return checkout_session
     except stripe.error.StripeError as e:
         raise e
+
+
+def send_course_update_notification(course_id: int) -> None:
+    subscribers = CourseSubscription.objects.filter(course_id=course_id).select_related("course", "user")
+
+    messages = list()
+    for subscriber in subscribers:
+        msg = mail.EmailMessage(
+            subject=f"Course '{subscriber.course.title}' Updated",
+            body="You receive this message because course you subscribe for was updated./n/nКоманда sky.school.com",
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            to=[subscriber.user.email,],
+        )
+        messages.append(msg)
+
+    if messages:
+        backend = mail.mailers.default
+        backend.send_messages(messages)
 
 
 if __name__ == "__main__":
@@ -68,4 +96,5 @@ if __name__ == "__main__":
     # print(session)
     # session = get_stripe_checkout_session_info(session_id="cs_123")
     # print(session)
+    # send_course_update_notification(1)
     pass
